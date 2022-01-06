372345b91a913312f960c7069c5d276a0dc3a120
kirillgolovko <kirillgolovko@yandex-team.ru>
2022-01-06 13:21:10 +0300

    kirillgolovko typo fix

--- buildSrc/src/main/java/Versions.java
+++ buildSrc/src/main/java/Versions.java
@@ -1,5 +1,5 @@
 public interface Versions {
     String guava = "31.0.1-jre";
-    // hopefuly, won't grant access to my pc for everyone
+    // hopefully, won't grant access to my pc for everyone
     String log4j = "2.17.1";
 }
