0ab2413c237a4bfa95ab1a95bb8eac9e63c29030
kirillgolovko <kirillgolovko@yandex-team.ru>
2022-05-13 18:49:05 +0300

    kirillgolovko hw-25 impl

---
+++ HW28-SpringDataJdbc/homework-template/src/main/java/ru/otus/crm/model/Phone.java
@@ -0,0 +1,60 @@
+package ru.otus.crm.model;
+
+import javax.annotation.Generated;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.MappedCollection;
+
+/**
+ * @author kirillgolovko
+ */
+public class Phone implements Cloneable {
+
+    @Id
+    private Long id;
+    private String number;
+
+    public Phone() {
+        this(null, null);
+    }
+
+    public Phone(Long id, String number) {
+        this.id = id;
+        this.number = number;
+
+    }
+
+    public Phone(String number) {
+        this(null, number);
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return new Phone(id, number);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Phone{" +
+                "id=" + id +
+                ", number='" + number + '\'' +
+                '}';
+    }
+}
